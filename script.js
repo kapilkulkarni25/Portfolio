d636060c262ee0c37ea273209a7ddb3ca6922805
kapilkulkarni25 <kapilkulkarni74@gmail.com>
2024-08-08 22:53:08 +0530

    feat: revamp

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("working");
-
 let themeDots = document.getElementsByClassName("theme-dot");
 
 for (var i = 0; themeDots.length > i; i++) {
@@ -23,3 +21,94 @@ function setTheme(mode) {
     document.getElementById("theme-style").href = "purple.css";
   }
 }
+
+VANTA.BIRDS({
+  el: "#waves-bg",
+  backgroundColor: 0xd2d56,
+  backgroundAlpha: 0.91,
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  colorMode: "variance",
+  birdSize: 1.10,
+  wingSpan: 19.00,
+  speedLimit: 4.00,
+  separation: 68.00,
+  alignment: 43.00,
+  cohesion: 1.00,
+  quantity: 4.00
+})
+
+// VANTA.WAVES({
+//   el: "#waves-bg",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   scale: 1.00,
+//   scaleMobile: 1.00,
+//   // color: 0x3988,
+//   color: window.getComputedStyle(document.documentElement).getPropertyValue('--previewShadow'),
+//   shininess: 65.00,
+//   waveHeight: 27.50,
+//   zoom: 0.89
+// })
+
+$(function () {
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+    if ($(".timeline-content").hasClass("js--fadeInLeft")) {
+      $(".timeline-content")
+        .removeClass("js--fadeInLeft")
+        .addClass("js--fadeInRight");
+    }
+
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800
+    });
+  } else {
+    sr.reveal(".js--fadeInLeft", {
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800
+    });
+
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800
+    });
+  }
+
+  sr.reveal(".js--fadeInLeft", {
+    origin: "left",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800
+  });
+
+  sr.reveal(".js--fadeInRight", {
+    origin: "right",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800
+  });
+});
+
+$('.post-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+});
